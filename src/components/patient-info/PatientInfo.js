e6e7afd532161737e54db4dac9344395fd4aa91c
Jamal U Din <jamaluddin14926@gmail.com>
2023-07-31 12:46:15 +0500

    Variables are added to store data entered by user. Final changes are done

--- src/components/patient-info/PatientInfo.js
+++ src/components/patient-info/PatientInfo.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import "react-phone-number-input/style.css";
-// import Input from 'react-phone-number-input/input'
-// import PhoneInput from "react-phone-number-input";
 import "./PatientInfo.css";
 import "./style.scss";
 import defaultUser from "./defaultUser.jpg";
@@ -9,17 +7,23 @@ import defaultUser from "./defaultUser.jpg";
 import NavigationBar from "../navbar/NavigationBar";
 
 export default function PatientInfo() {
-  const [value, setValue] = useState();
-  const [val, setVal] = useState();
+  
   const [enrolledTime, setEnrolledTime] = useState("09:00 AM");
   const [OPD, setOPD] = useState("B112");
   const [doctor, setDoctor] = useState("Dr. Adam J.");
+  const [fullName, setFullName] = useState();
+  const [contact, setContact] = useState();
+  const [CNIC, setCNIC] = useState();
+  const [DOB, setDOB] = useState();
+  const [mail, setMail] = useState();
+  const [Emaildomain, setEmailDomain] = useState();
+  const [temperatur, setTemperature] = useState();
+  const [BP, setBP] = useState();
+  const [scale, setScale] = useState();
+  const [PR, setPR] = useState();
 
   const [isOpen, setIsOpen] = useState(false);
   const [medicalHistory, setMedicalHistory] = useState();
-  // const [isActive, setIsActive] = useState(true);
-
-  // setEnrolledTime("");
 
   const MedicalHistory = () => {
     setMedicalHistory(document.getElementById("history").value);
@@ -35,6 +39,22 @@ export default function PatientInfo() {
     console.log(medicalHistory);
   };
 
+  const TempScale = () => {
+    if(document.getElementById('Celcious').checked)
+    {
+      setScale('C')
+    }
+    else if(document.getElementById('Faren').checked)
+    {
+      setScale('F');
+    }
+  }
+
+  const SaveData = () => {
+    setEnrolledTime(enrolledTime);
+    setOPD(OPD);
+    setDoctor(doctor);
+  }
 
   return (
     <>
@@ -76,22 +96,14 @@ export default function PatientInfo() {
               name="Full_name"
               id="FullName"
               placeholder="Muhammad Hassan Waheed"
+              value={fullName}
+              onChange={e => setFullName(e.target.value)}
             />
           </div>
 
           <div className="row">
             <label htmlFor="phone">Contact No#:</label>
-            <input type="text" className="phone" id="phone" placeholder="+92 345 7442650" value={value} onChange={setValue} />
-            {/* <PhoneInput
-              className="phone"
-              id='phone'
-              international
-              defaultCountry="PK"
-              placeholder="345 7442650"
-              displayInitialValueAsLocalNumber
-              value={value}
-              onChange={setValue}
-            /> */}
+            <input type="text" className="phone" id="phone" placeholder="+92 345 7442650" value={contact} onChange={e => setContact(e.target.value)} />
           </div>
 
           <div className="row">
@@ -99,17 +111,15 @@ export default function PatientInfo() {
             <input
               type="tel"
               pattern="[0-9]*"
-              value={val}
-              onChange={(e) =>
-                setVal((v) => (e.target.validity.valid ? e.target.value : v))
-              }
+              value={CNIC}
               id="CNIC"
               placeholder="31101-5694352-2"
+              onChange={e => setCNIC(e.target.value)}
             />
           </div>
           <div className="row">
             <label htmlFor="dob">DOB:</label>
-            <input type="date" name="DOB" id="dob" />
+            <input type="date" name="DOB" id="dob" value={DOB} onChange={e => setDOB(e.target.value)} />
           </div>
 
           <div className="row">
@@ -119,11 +129,13 @@ export default function PatientInfo() {
               name="Email"
               id="email"
               placeholder="hassanwaheed_12"
+              value={mail}
+              onChange={e => setMail(e.target.value)}
             />
-            <select name="domains" id="domain">
-              <option value="Gmail" defaultValue>@gmail.com</option>
-              <option value="Yahoo">@yahoo.com</option>
-              <option value="Yahoo">@hotmail.com</option>
+            <select name="domains" id="domain" onChange={e => setEmailDomain(e.target.value)}>
+              <option value="@gmail.com" defaultValue>@gmail.com</option>
+              <option value="@yahoo.com">@yahoo.com</option>
+              <option value="@hotmail.com">@hotmail.com</option>
             </select>
           </div>
         </div>
@@ -134,23 +146,23 @@ export default function PatientInfo() {
           <div className="vital_col">
             <div>
               <label htmlFor="temp">Temperature: </label>
-              <input type="text" id="temp" placeholder="00.00" />
+              <input type="text" id="temp" placeholder="00.00" value={temperatur} onChange={e => setTemperature(e.target.value)} />
             </div>
-            <input type="radio" name="temperature" id="Celcious" />
+            <input type="radio" name="temperature" id="Celcious" value="C" onChange={TempScale} />
             <label htmlFor="Celcious">C</label>
-            <input type="radio" name="temperature" id="Faren" />
+            <input type="radio" name="temperature" id="Faren" value="F" onChange={TempScale} />
             <label htmlFor="Faren">F</label>
           </div>
           <div className="vital_col BP_col">
             <label htmlFor="BP">Blood Pressure: </label>
-            <input type="text" id="BP" placeholder="00.00" />
+            <input type="text" id="BP" placeholder="00.00" value={BP} onChange={e => setBP(e.target.value)} />
           </div>
           </div>
 
           <div className="vital_row">
           <div className="vital_col PR_col">
             <label htmlFor="PR">Pulse Rate: </label>
-            <input type="text" id="PR" placeholder="00.00" />
+            <input type="text" id="PR" placeholder="00.00" value={PR} onChange={e => setPR(e.target.value)} />
           </div>
           <div className="vital_col request_col">
             <a href="/" onClick={()=>{
@@ -171,7 +183,9 @@ export default function PatientInfo() {
         {/* Navigation Buttons */}
         <div className="nav_buttons">
           <button className="back">Back Home</button>
-          <button className="done">Done</button>
+          <button className="done" onClick={()=>{
+            SaveData();
+          }}>Done</button>
         </div>
       </div>
 
